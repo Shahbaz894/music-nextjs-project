d0a6354eb8b0a66f1eac22916f39bb23e381132c
shahbaz <shahbazzulfiqar894@gmail.com>
2024-09-22 20:20:14 +0500

    create card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCoureses from "@/components/FeaturedCoureses";
 import HeroSection from "@/components/HeroSection";
 import Head from "next/head";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased 
     bg-grid-white/[0.2]">
     <HeroSection/>
+    <FeaturedCoureses/>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { Spotlight } from './ui/spotlight';
+import { Spotlight } from './ui/Spotlight';
 import { Button } from './ui/moving-border';
 
 const HeroSection = () => {
